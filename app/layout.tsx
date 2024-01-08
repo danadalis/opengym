c2cd6c7263f5a4ae25565b4f6b8b95a7db0a1491
Daniel Dahlström <danadalis@icloud.com>
2024-01-08 18:38:10 +0100

    new index page

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'OpenGym',
+  description: 'Jämför och hitta bästa gymsystemet för ditt CrossFit gym',
 }
 
 export default function RootLayout({
