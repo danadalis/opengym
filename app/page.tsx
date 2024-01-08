a9a2b37fb200443d6277933a721a5f51486c516c
Daniel Dahlström <danadalis@icloud.com>
2024-01-08 19:14:57 +0100

    remove template content

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
-import Header from '@/components/Header'
 import { cookies } from 'next/headers'
 
 export default function Index() {
@@ -29,7 +26,6 @@ export default function Index() {
       {/* This is where the "OpenGym" heading from your HTML goes */}
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h1 className="text-clamp text-center font-bold">OpenGym</h1>
         </main>
